042b0eaf9482ad2b8c5f0416d12bc18a79fea2e6
dwaynediddy <burrows.dwayne@icloud.com>
2022-03-09 15:29:04 +0100

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        <h2>Start Quizzle</h2>
+        <button>Start</button>
+    </div>
+    
+  )
+}
+
+export default Home
